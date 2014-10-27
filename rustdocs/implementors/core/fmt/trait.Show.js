948a2fd7dafd545d1d77ce8d2fd14b3c6880d4c3
Gary M. Josack <gary@byoteki.com>
2014-10-27 01:09:05 -0700

    Add pgs_files docs

---
+++ rustdocs/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pgs_files'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='pgs_files/passwd/struct.PasswdEntry.html' title='pgs_files::passwd::PasswdEntry'>PasswdEntry</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='pgs_files/group/struct.GroupEntry.html' title='pgs_files::group::GroupEntry'>GroupEntry</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='pgs_files/shadow/struct.ShadowEntry.html' title='pgs_files::shadow::ShadowEntry'>ShadowEntry</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rustdocs/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pgs_files'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;T: <a class='trait' href='pgs_files/trait.Entry.html' title='pgs_files::Entry'>Entry</a>&lt;T&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;T&gt; for <a class='struct' href='pgs_files/struct.Entries.html' title='pgs_files::Entries'>Entries</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
