035240e7a7f50fc8dcf39a423182474633a7b5ef
Gary M. Josack <gary@byoteki.com>
2015-08-03 00:47:00 -0700

    Fixes and scaffolding
    Fixed some links to be absolute
    Started laying out simple blogging

--- src/make.py
+++ src/make.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 
+import logging
+import glob
+
 from jinja2 import Environment, FileSystemLoader
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.styles import get_style_by_name
+from pygments.formatters import HtmlFormatter
+
+
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            return "\n<pre><code>%s</code></pre>\n" % mistune.escape(code)
+
+        style = get_style_by_name("monokai")
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter(style=style)
+
+        return highlight(code, lexer, formatter)
 
 
 class MakeEnvironment(object):
     def __init__(self):
         self.template_env = Environment(loader=FileSystemLoader("templates"))
+        self.markdown = mistune.Markdown(renderer=HighlightRenderer())
 
     def write_template(self, template_name, dest, ctxt=None):
+        logging.info("Writing out generated file (%s)", dest)
         if ctxt is None:
             ctxt = {}
         template = self.template_env.get_template(template_name)
@@ -15,13 +37,29 @@ class MakeEnvironment(object):
         with open(dest, "w") as out_file:
             out_file.write(out)
 
+    def generate_blog_post(self, src):
+        with open(src) as post_file:
+            text = post_file.read()
+        return self.markdown.render(text)
+
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
     make_env = MakeEnvironment()
 
     make_env.write_template("index.html", "../index.html")
     make_env.write_template("blog.html", "../blog.html")
     make_env.write_template("resume.html", "../resume.html")
+
+    for post in glob.glob("blog-posts/*.md"):
+        post_html = make_env.generate_blog_post(post)
+        post_name = post.rsplit("/")[-1][:-3]
+        make_env.write_template(
+            "blog-post.html",
+            "../blog/{}.html".format(post_name),
+            {"post": post_html}
+        )
 
 
 if __name__ == "__main__":
